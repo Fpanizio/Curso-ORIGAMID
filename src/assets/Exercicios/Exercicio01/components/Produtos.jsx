6e96dcb704cc71379e43c1973c9a4e0234e1048e
Felipe Panizio <fpanizio10@gmail.com>
2024-11-18 11:14:22 -0300

    Finalizando parte de reactHooks

--- src/assets/Exercicios/Exercicio01/components/Produtos.jsx
+++ src/assets/Exercicios/Exercicio01/components/Produtos.jsx
@@ -2,16 +2,28 @@ import React, { useContext } from "react";
 import { GlobalContext } from "./GlobalContext";
 
 const Produtos = () => {
-  const {dados} = useContext(GlobalContext);
+  const { dados } = useContext(GlobalContext);
   // console.log(global);
 
   if (dados === null) return null;
   return (
     <>
       <div>
-        <div>
+        <div
+          style={{
+            display: "grid",
+            gridAutoFlow: "column",
+            gridTemplateColumns: "repeat(3 minmax(100px, 1fr)",
+            gridTemplateRows: "repeat(3, 100px)",
+            alignItems: 'center',
+          }}
+        >
           {dados.map((item) => (
-            <p key={item.id}>{item.nome}</p>
+            <div key={item.id}>
+              <p style={{display: "flex", justifyContent: "center"}}>
+                {item.nome} - R$ {item.preco}
+              </p>
+            </div>
           ))}
         </div>
       </div>
